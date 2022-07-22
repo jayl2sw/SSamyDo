788ed017fbe361063637543fdc7ad33f83376542
jayl2sw <jayl2sf@gmail.com>
2022-07-22 16:28:09 +0900

    UPDATE DOMAIN fix bugs on UserDomain & Track -lee
    Former-commit-id: 496593e64dcd4b57ac6532457897383e06a6b253

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/UserDomain.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/UserDomain.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:877026cd10b19a6af34ee581f173b289e073e6f12ae8cfe6ec284b2dd5ac43d0
-size 501
+oid sha256:142d895ad90dd2248f1f0599b7eade453fa0562c958c8a17ba52e4bb383cdc32
+size 499
