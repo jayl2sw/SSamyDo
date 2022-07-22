c8fbb10454c6a0fc4998142e08122d6f8810a827
JHB <jo7hb@naver.com>
2022-07-22 17:52:43 +0900

    UPDATE UserAPI Update setUserEduInfo -jo
    Former-commit-id: b4b9aeb1c637afa9fe4347edd5ede82fb19615a5

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/UserDomain.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/UserDomain.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bafe69b6653e7aef231c64f976b1b81b4e40684f9c5a6ce1541ab8ff3f52cca
-size 562
+oid sha256:644bf12234938caf339843064d85016bf62936a7711d6ef1c12cc9749447dd15
+size 673
---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/UserRepositoryImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:711e1d0656cd007b959aa5ef4a8b38580ba080bbded1e0b4093c7b17f8d41a3e
+size 924
