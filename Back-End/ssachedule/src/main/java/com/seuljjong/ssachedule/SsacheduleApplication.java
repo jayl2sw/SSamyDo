c7b3521d761b83e76d474d930c5167698d466a14
jayl2sw <jayl2sf@gmail.com>
2022-07-22 13:00:23 +0900

    UPDATE README add ERD #lee
    Former-commit-id: 043d047e24e752a0fb30d6421f5648318eb67c9f

--- Back-End/ssachedule/src/main/java/com/seuljjong/ssachedule/SsacheduleApplication.java
+++ Back-End/ssachedule/src/main/java/com/seuljjong/ssachedule/SsacheduleApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fc2ca3eeeb99d14c525d54d23719fdc21a254d267a5e934668f4cf42c8e4bdbb
-size 338
+package com.seuljjong.ssachedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsacheduleApplication.class, args);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a9e7ce02b8e9dcb67975d704914558ee03c88b9f96da167437351205c4b125a
-size 11215
+oid sha256:a3fd6fb062475cb06f4eb8071574db5029fee7ce67d24b422cfb4edaca33817c
+size 11354
