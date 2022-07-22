1c7b60f77cac1f4d19ffb83b1236f17a1c3d5524
JHB <jo7hb@naver.com>
2022-07-22 22:16:06 +0900

    ADD Crawling Module Add crawling module to get userInfo and schedule at edu.ssafy.com
    Former-commit-id: 564717151dc8e198d8410ec4238b416d2f33dcb3

---
+++ Back-End/crawling/src/App.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:389c681d083d6372ae99a4eeeb8969b9a232102e7d8a25fc62d27b18ed9a7095
+size 6352
---
+++ Back-End/crawling/src/Box.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c6c3e9262903fd27f048ec603cdb3063e6f10c0c0d3338eb968bf1bfd6fb9a6
+size 1870
