296d699abddedcbcdd1d886fe477d93a293fdc4b
jayl2sw <jayl2sf@gmail.com>
2022-07-27 10:42:18 +0900

    ADD lunch repository add lunch repository #lee
    Former-commit-id: 7cae60521127deed12e18f858c0043da06d9b5cc

---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/LunchRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ba24de1165ba641a10f917c4504fe256c54bba66a192dedf9e3af008d7373f0
+size 322
---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/LunchRepositoryImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1a8f11effc2d0ebb57929aad1fdfb71942188591471f9ed6ccdd01fba46cd2f
+size 675
