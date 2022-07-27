02a09a8a8678285694c89cc6e7e7d9a375cbde1b
JHB <jo7hb@naver.com>
2022-07-27 16:00:23 +0900

    UPDATE MealCrawler Fix code. Adjust the timing of the driver opens.
    Former-commit-id: 69d55d70546f614050b7f079fcfc0822ff79a2ef

--- Back-End/mealcrawler/mealcrawler.py
+++ Back-End/mealcrawler/mealcrawler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bf791a22e3c8aeb010ae6622532ca6c99952c251acf0dcd2b750f788ed19e9a
-size 5158
+oid sha256:dcf64db46622865d51b98a865dcb52768cabc727ad07af1229a260f4bc035744
+size 5152
