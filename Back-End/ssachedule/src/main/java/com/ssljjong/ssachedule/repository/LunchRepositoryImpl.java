9457be78245d937b42850d613bc35ce3e52dad0b
jayl2sw <jayl2sf@gmail.com>
2022-07-27 11:37:31 +0900

    ADD lunch service UPDATE lunch repository add method to find lunch list for a certain period of time #lee
    Former-commit-id: 0b4b2848caeb583b9eb16976e7e950a12b7225c2

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/LunchRepositoryImpl.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/LunchRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9710aa3874f73a24e9909bdbd2c9c1f958d679f6fcca0505c300d88c6d8d29d1
-size 649
+oid sha256:0be921d51dbbbf22d59e2e65e5503151b995a992178034c65f462ac91f9b314c
+size 845
---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/LunchServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4db91c3b64567d8ed0ab8e99df1a2dbcdad7f22d946b367a546f9de3089df9df
+size 1550
