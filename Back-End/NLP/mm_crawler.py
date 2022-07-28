d5249dd18102689410b4f0e744c9e69a0caebb7b
jaejun lee <jayl2cu@gmail.com>
2022-07-28 11:33:33 +0900

    UPDATE mm_crawler change method to find channels by user and unlimit number of posts in one page and delete unneccessary posts #lee
    Former-commit-id: ee02862fdbdb499c42d823cfc1b005e06e985471

--- Back-End/NLP/mm_crawler.py
+++ Back-End/NLP/mm_crawler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46e1ce19262ab13326adf43af25d63417d72b8f8ac67fe59fbd0a2ea9d2095ca
-size 1031
+oid sha256:8fb4810790ba44c543e76da5a961e4237be857898f210b5e393941cbe77ebe16
+size 1417
