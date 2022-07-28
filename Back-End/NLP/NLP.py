aebda1892452c74552f0ffa7baf278351c685b71
jaejun lee <jayl2cu@gmail.com>
2022-07-28 23:44:03 +0900

    ADD nlp module still need to tune #lee
    Former-commit-id: 43741d004f934b42abcf7e4cb5556d496b4d01e1

---
+++ Back-End/NLP/NLP.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee02faf6e69ad8d804a7dc9d7c4febbeaec5c213890dfc5203824b6189a14f71
+size 9901
--- Back-End/NLP/mm_crawler.py
+++ Back-End/NLP/mm_crawler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb4810790ba44c543e76da5a961e4237be857898f210b5e393941cbe77ebe16
-size 1417
+oid sha256:572e236325f269c6feafd5173d1e7753a7dfae85cdf52a461d911417bd4445e0
+size 1504
