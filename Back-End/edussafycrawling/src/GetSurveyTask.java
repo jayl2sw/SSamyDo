6fd3856ab8c615345de9ae745c460ad52d34683d
JHB <jo7hb@naver.com>
2022-07-29 13:50:00 +0900

    UPDATE EduSsafyCrawling. Add new thread task for notification crawling. -jo
    Former-commit-id: be9807fb563428c48e5b26b9ac720b45599c53ef

--- Back-End/edussafycrawling/src/GetSurveyTask.java
+++ Back-End/edussafycrawling/src/GetSurveyTask.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e484411525d862d72c68f16b2a2c7dd0d5545c9da4e4d162517ba008df8e7667
-size 1667
+oid sha256:42a744745c3b794b99a077fc2de3e9b1de685e30ba8fe3dd59a2f709f53fe315
+size 1593
---
+++ Back-End/edussafycrawling/src/Notification.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a9ada4046f3a5101ae2415aaa65b20758ecd49b01f9f66f3b13d79d3ac0d364
+size 2110
