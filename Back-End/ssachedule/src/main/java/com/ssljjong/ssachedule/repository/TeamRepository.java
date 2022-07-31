34ea1a8a995e4dae57d19473624581837781fa1f
이재준 <jayl2cu@gmail.com>
2022-07-31 17:57:34 +0900

    ADD services add all services without UserService need to add Authorization on  UserService add LunchController need some Data for Testing #lee
    Former-commit-id: 45739960839f37c0c87d56371750864a4b8a0321

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TeamRepository.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TeamRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c010fccb119ee236bc4d173a27cf4ed8756d99ff33a4e80d3dd1b9136f95ea36
-size 794
+oid sha256:8b033bf706886ad976e71846f40963bc28af3d93341ccf60abb814fca200c6b3
+size 793
