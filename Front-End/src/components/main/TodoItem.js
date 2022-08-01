717109c5bb1833518d141c1435b3269b1eb2764d
ms_google <ms_google@naver.com>
2022-08-01 17:36:36 +0900

    REMOVE main style remove style.center from main.js #park
    Former-commit-id: 99c854d7e558569dd00e513336e3c67f0011ae7b

--- Front-End/src/components/main/TodoItem.js
+++ Front-End/src/components/main/TodoItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ea6765c9bce2c9d9c268ceb79175e71e6acb6e2652b409be8b51dacc8aa5203
-size 965
+oid sha256:02736209c4b7592afc931d05e763983c3b4da98a06c13dae6fbc206faa8a7fab
+size 956
