df1b2d9b25388c5694cc19eb91139b69c65e893f
조현빈 <jo7hb@naver.com>
2022-08-01 09:03:14 +0900

    Update
    Former-commit-id: 507732e1f6909f053ada8a90eaff840cf4690ee8

--- Back-End/NLP/mm_crawler.py
+++ Back-End/NLP/mm_crawler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46e1ce19262ab13326adf43af25d63417d72b8f8ac67fe59fbd0a2ea9d2095ca
-size 1031
+oid sha256:42c4cd17b20d3c0fc2ba8eb33a6be79cce0e8a60f59205024ddd8de275f34934
+size 1049
