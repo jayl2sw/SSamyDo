ff885e7dddb88d4b1bf1b07b735d80c7a5748a47
ms_google <ms_google@naver.com>
2022-08-01 17:36:36 +0900

    REMOVE main style remove style.center from main.js #park
    Former-commit-id: ae4313c34157ffd6826791ec86463c6509dcbfe0

--- Front-End/src/components/main/TodoList.js
+++ Front-End/src/components/main/TodoList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff21bef578631dc238ea78a4efd76cf99567fc560c4b5e1b1c73c44c6de2fe2b
-size 875
+oid sha256:1da4dffa4059c00f7b624658bb6397863cfadb5b54fc8b6651bfdc94258ddd8b
+size 899
