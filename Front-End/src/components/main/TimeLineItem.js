1c793e939b5df0c3435951577c8ea61f25c25e1a
hyunspace <at.hyunspace@gmail.com>
2022-08-03 16:39:56 +0900

    ADD TimeLine timeline date buttons and timeline code #kim
    Former-commit-id: 047c379532f7bfd23144ac273ec25304a0bbc1de

--- Front-End/src/components/main/TimeLineItem.js
+++ Front-End/src/components/main/TimeLineItem.js
@@ -1,10 +1,3 @@
-import { View, Text } from "react-native";
-import styles from "../../../app.module.css";
-
-export default function TimeLineItem() {
-  return (
-    <View>
-      <Text>TimeLineItem.js</Text>
-    </View>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1974cac64da77f942b168404e91d1a268dd866d363c0d57d3af2430ca2a0acfd
+size 2420
