53fb4466753369d96aaa1251b6e0b222a92acc38
jaejun lee <jayl2cu@gmail.com>
2022-08-04 14:49:33 +0900

    ADD signup add signup #lee
    Former-commit-id: 86ee73d391de3476ef3216cd66bad96f6d905353

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/UserDto.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/UserDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee456196a2aa4ead3ba15abef8676a754bc84e73f3514ef8befdca325eba215a
-size 1159
+oid sha256:8ef20f67b8371dee83e224654976505477614496ada726d7900063a3df861a9a
+size 1146
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/UserService.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f41a91b45a5f0d52246a0cd7a6821ddf98b2a5c193b186d0a7d7046bb8fe8fb
-size 3814
+oid sha256:34d38d6604d29600067e37fa8d6ce5ba15dc4d436fc25ba44aff7f885c9079c3
+size 3838
