d41df59ac275deebf36bf9e8a60e3357cd6e4e71
전현우 <soshot22@naver.com>
2022-08-09 12:05:19 +0900

    ADD SIGNIN add signin #jeon
    Former-commit-id: ae74b5d7b8877046c704248dba8674456e4e3dd5

--- Front-End/src/components/signin/Aggrement.js
+++ Front-End/src/components/signin/Aggrement.js
@@ -1,10 +1,3 @@
-import { View, Text } from "react-native";
-import styles from "../../../app.module.css";
-
-export default function Aggrement() {
-  return (
-    <View>
-      <Text>Aggrement.js</Text>
-    </View>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c64a44ba14121912dbb1cad0ba6639158d627dd5751e93dc522ca1931a3f267
+size 1861
