76bcd955b2776f5cfba4650161f9d7e286a25861
전현우 <soshot22@naver.com>
2022-08-11 17:44:22 +0900

    FIX LOGIN fix login #jeon
    Former-commit-id: e53a2ea158ad83a1eb334feb41255c963c171823

--- Front-End/src/components/signin/Verification.js
+++ Front-End/src/components/signin/Verification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e0fb60810fb57b2830d063ab89b5dbef7a86539f69acb5cc287481619117fc7
-size 4254
+oid sha256:fb1a93b5f3d5f01f394df5fcebe5967139ffbeb2f724ed0bdef01e434693bd39
+size 7680
