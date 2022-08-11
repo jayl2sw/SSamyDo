123617816e4497eb6b3e7c324b6d297841f8df3b
33j073 <33j073@gmail.com>
2022-08-11 15:46:58 +0900

    ADD timeline styles timeline component styling #kim
    Former-commit-id: 349080b058b5c3654b9578f12d164314f117d841

--- Back-End/edussafycrawling/src/App.java
+++ Back-End/edussafycrawling/src/App.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:becde1952adbe43eea0e957479b5d7366c668fb1c1308fb94ca11f4393378880
-size 2449
+oid sha256:5d7e0b418685ee7720d90d03314ca5f8710e2331538d11dd29e8a9a0ee87dab7
+size 2382
