08fea449ce7feecf70440a480c3e654ad2021ccc
33j073 <33j073@gmail.com>
2022-08-13 02:50:18 +0900

    REFACTOR Timeline axios refactor axios to fetch weekly schedule data #kim
    Former-commit-id: 2172fdd38ff292831af87a044d1a5423f6508d31

--- Front-End/src/components/main/TimeLine.js
+++ Front-End/src/components/main/TimeLine.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbc5619163885f408e6a514cc639b20c49b912c982fb2e1d5de0481d2228334c
-size 8050
+oid sha256:ca367b2a8b27ebfa589e637d61ca6ee4d3731b475602b94e0722dc951a85ba7e
+size 7745
