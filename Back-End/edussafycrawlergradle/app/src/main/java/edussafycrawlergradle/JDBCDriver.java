e6fa1c99d0cf292f294de54d54d9023d2a5503c9
조현빈 <jo7hb@naver.com>
2022-08-12 10:59:01 +0900

    FIX database column Add Column startdate for todo table - jo
    Former-commit-id: 4f9fd9af392b1a27495e39b73a4535e667230493

--- Back-End/edussafycrawlergradle/app/src/main/java/edussafycrawlergradle/JDBCDriver.java
+++ Back-End/edussafycrawlergradle/app/src/main/java/edussafycrawlergradle/JDBCDriver.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efdec4c1e8644dab749b5e8718744f67c16543ffb0699ab1c4bdce52eeb558e1
-size 6477
+oid sha256:5134de34fb656fe4d2f5b39a8f39c4e2ac8576dc5e0d5783cdc65108a53e03c5
+size 6584
