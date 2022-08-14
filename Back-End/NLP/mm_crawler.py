e39b7184912dc5b114fd15eaba051440cde517d4
이재준 <jayl2cu@gmail.com>
2022-08-14 20:34:41 +0900

    UPDATE django server
    Former-commit-id: 18e8e76982dcb8efad3659b01d190f91d1ce6644

--- Back-End/NLP/mm_crawler.py
+++ Back-End/NLP/mm_crawler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7320dcc2dcc01938ce235b711b78a32cf4f6b8c1470a2bbdfddcfe09523b0c12
-size 2423
+oid sha256:610a71f98b18937a2ba208a34dd9e00692c8b606f9297cac29d459fc0d4dce5c
+size 3033
