be5ac67ce806e19cac3256d867788d6b292b3d30
이재준 <jayl2cu@gmail.com>
2022-08-14 20:35:11 +0900

    UPDATE django server
    Former-commit-id: cb7931804f93423c250edf19bd9b03493a035599

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e6d88e1d126d0c0d0a2aaaefc7ed8ad5458acdf54b29a8a3b54693382aa6f18
-size 3175
+oid sha256:8e652479e3897e7706bccd784f7aebbe0b2e1b6ef54ba778dc9bb02a472d118a
+size 3183
