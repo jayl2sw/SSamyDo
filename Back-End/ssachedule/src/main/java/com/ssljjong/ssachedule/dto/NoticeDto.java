b7f9a2485717ac9f255bef3820573dd1bcb04732
이재준 <jayl2cu@gmail.com>
2022-08-18 00:19:14 +0900

    UPDATE usercontroller
    Former-commit-id: b10132d23a02f9a771fca7f607b9c1cb7c20ad65

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/NoticeDto.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/NoticeDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2221604e8742c46058c94ca2cfc570141531721a229c84443c7b1a9082e76c4
-size 710
+oid sha256:ab910d9f94139e87bca2573e9e5250f42ecc6c0f0da88b6061af0cefbe49888d
+size 711
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/entity/Notice.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/entity/Notice.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d9bcbbcaf15ef1f3f5f77b11278df6f34287ec8595f36615458e8109eb000b2
-size 909
+oid sha256:39a65ad6664dfa5de77efb955465bc41b0c5e71ee8a58e6529992637a63aff76
+size 1364
