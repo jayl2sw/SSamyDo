7164fdbb6835eac85833cd304d1a505d7bff2f69
33j073 <33j073@gmail.com>
2022-08-17 18:24:28 +0900

    ADD Lunchboard CSS Lunchboard page styling #kim
    Former-commit-id: cf217d21fa7966caa14f5f5efc9d54316a0e9825

--- Front-End/src/components/signin/Verification.js
+++ Front-End/src/components/signin/Verification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bb503a7859bdfb98bfac573f052e68e697f14a49b40ccf06862e93d48c50026
-size 14063
+oid sha256:3bf850c595e082734117fb221d230b062f59f13cbbaca15cf434098d968e04a6
+size 14065
