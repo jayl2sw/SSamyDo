34455def81bf2950c964cb15c8d2cefcddbcdb11
이재준 <jayl2cu@gmail.com>
2022-08-17 12:09:02 +0900

    FIX conflicts
    Former-commit-id: 8ca85791c23ea8da882a3afc376b05772f3d8416

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:004b57c0a2fdc275b3493cae9d31fd8dc50dad67f1b4c9f1f90b226c0f6461ee
-size 3531
+oid sha256:cdc84a77d1d7c76170b7030fcc2ca2d3a3828d7a3796c08ed1addcf85ed52452
+size 2599
--- Front-End/App.js
+++ Front-End/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:219dd0152f4cae802e78ec48e60ec7508bd7d5586dfca44371caf5208d56332f
-size 1065
+oid sha256:8b8d3c7fdc736a8dde1e930cc329a41334ba3999ac4cac4bee3a8943393e3191
+size 1026
