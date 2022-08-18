e2354fc85117f083d7ce001b6d6b0c2d127575c9
이재준 <jayl2cu@gmail.com>
2022-08-18 21:34:50 +0900

    UPDATE django preprocess server finished debug #lee
    Former-commit-id: b703baa7cedc11a20d4ae3e2e5ed709c25be1ec3

---
+++ Back-End/preprocess/manage.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:379a5cae29e787838875d2a30a670144b24c1e80b705e8d8a573927d7ede327c
+size 688
--- Back-End/preprocess/preprocess/nlp/migrations/0001_initial.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:319c938975e62c05ed7ae652a4c2b91acafca2948346ac9714aa8f82c0d3b6cc
-size 756
