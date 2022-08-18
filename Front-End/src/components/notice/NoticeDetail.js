77226fd379711fdf0013bce12b7a953b49953231
ms_google <ms_google@naver.com>
2022-08-19 06:32:27 +0900

    FIX noticedetail button change #park
    Former-commit-id: 75e7040e15eea5c9dc57a438f20edbe0d38369d4

--- Front-End/src/components/notice/NoticeDetail.js
+++ Front-End/src/components/notice/NoticeDetail.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c39fe0104d0e0f860e5a8ad762eae27773a95a815ef477723238ebbdd04746c1
-size 5062
+oid sha256:01e002c3df73c2718e20a98fd947841c51a70b0d13f9749a2ed17587063521ef
+size 5599
