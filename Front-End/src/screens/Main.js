a610f91e3f876ac1c7b63d9d1c1ad6f0ad289caa
33j073 <33j073@gmail.com>
2022-08-18 16:08:43 +0900

    ADD Lunchboard title Add lunchboard title #kim
    Former-commit-id: 5ecdb72ef5151258d24c4674cba4f7f06319b05b

--- Front-End/src/screens/Main.js
+++ Front-End/src/screens/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:951fdf642e165e7ee15e3b2809f47897c7e127b48e9c8a606970900b2a6c5a1e
-size 3636
+oid sha256:5abb5a75126fcc231008d56cb7e610087a22a3a06f95eef25e2acecf5e1a2101
+size 3216
