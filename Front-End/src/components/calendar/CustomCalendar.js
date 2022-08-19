ae4bd5bcc2f008efb683902d6c9d54e6b32b50c7
33j073 <33j073@gmail.com>
2022-08-19 10:00:25 +0900

    REMOVE Unsed variables Delete unsed varibales & code #kim
    Former-commit-id: 47c539e0e6fd06d23e41082513f00be2f58e72cc

--- Front-End/src/components/calendar/CustomCalendar.js
+++ Front-End/src/components/calendar/CustomCalendar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14c6da6b767e6a85e47d6df2f104c099f73a517e247fe7c39cc601505d319d97
-size 4794
+oid sha256:301bb995f57604045696534fbea866d8a783e8e12531bbc1c89798a92f746daf
+size 4374
