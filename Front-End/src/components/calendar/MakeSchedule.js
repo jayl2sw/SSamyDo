1cf3d03481fe934f2a0421eb788744f82209b3db
33j073 <33j073@gmail.com>
2022-08-19 10:05:21 +0900

    ADD MakeSchedule CSS Add errorContainer styling on MakeSchedule page #kim
    Former-commit-id: bf612b80e910799b9eb864b4b2c20d6b68301584

--- Front-End/src/components/calendar/MakeSchedule.js
+++ Front-End/src/components/calendar/MakeSchedule.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d25a6ebd8807806320472accc2a57191ee7ad7cfb8cbbcef508f2fbe086ee37
-size 13694
+oid sha256:332450b7f52f61493d971c878bab49d3d82622fca888eac610607e8ff140f83e
+size 14136
