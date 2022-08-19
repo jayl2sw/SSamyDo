9b2c0177819bd6faaf221f9f2f0fb04e9fbe780e
이재준 <jayl2cu@gmail.com>
2022-08-19 09:53:21 +0900

    UPDATE django server
    Former-commit-id: 539317d67d2df1790dc9ffa376193faf7e0d8cf4

--- Back-End/preprocess/nlp/views.py
+++ Back-End/preprocess/nlp/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b549465aa863b2cc38b4e35a5ed62dadff3398833f327d0cffae9a2a10b0b1ed
-size 6551
+oid sha256:78516b7f5b19b85b4d662a8180f564a93197e685352b06af8d93c2f4526152b2
+size 7253
